4d21cc735a3932b78631fb4ab3aadc9d0d62a60c
JinhengZhang <49140379+JinhengZhang@users.noreply.github.com>
2020-06-30 09:33:00 +0800

    Update colorchecker.cpp

--- src/colorchecker.cpp
+++ src/colorchecker.cpp
@@ -1,7 +1,8 @@
 #include<iostream>
 #include "colorspace.h"
-#include "utils.h"
+//#include "utils.h"
 #include "IO.h"
+#include "colorchecker.h"
 #include "opencv2\opencv.hpp"
 #include "opencv2\core\core.hpp"
 
@@ -10,17 +11,7 @@
 using namespace std;
 using namespace cv;
 
-class ColorChecker
-{
-	public:
-		Mat lab;
-		IO io;
-		Mat rgb;
-		char cs;
-		Mat white_mask;
-		Mat color_mask;
-		ColorChecker(Mat color, char colorspace, IO io_, Mat whites);
-}
+
 
 ColorChecker::ColorChecker(Mat color, char colorspace, IO io_, Mat whites)
 {
@@ -55,21 +46,6 @@ ColorChecker::ColorChecker(Mat color, char colorspace, IO io_, Mat whites)
 }
 
 
-class ColorCheckerMetric
-{
-public:
-	char cc;
-	char cs;
-	IO io;
-	Mat lab;
-	Mat xyz;
-	Mat rgbl;
-	Mat lab;
-	Mat grayl;
-	Mat white_mask;
-	Mat color_mask;
-	ColorCheckerMetric(ColorChecker colorchecker, char colorspace, IO io);
-}
 
 ColorCheckerMetric(ColorChecker colorchecker, char colorspace, IO io)
 {
