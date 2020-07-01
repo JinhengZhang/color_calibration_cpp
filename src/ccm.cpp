9fa158e700947587fb8c35aebfca376071cd25fc
JinhengZhang <49140379+JinhengZhang@users.noreply.github.com>
2020-07-02 00:13:25 +0800

    Update ccm.cpp

--- src/ccm.cpp
+++ src/ccm.cpp
@@ -1,85 +1,24 @@
-#include<iostream>
-#include<cmath>
-//#include "utils.h"
-#include "colorspace.h"
-#include "colorchecker.h"
-#include "opencv2\opencv.hpp"
-#include "opencv2\core\core.hpp"
-
-using namespace std;
-using namespace cv;
+#include "ccm.h"
 
 
-#define COLORSPACE(colorspace) colorspace() 
-#define COLORCHECKER(colorchecker) ColorChecker_##colorchecker() 
-#define LINEAR(linear) Linear_##linear(float gamma_, int deg_, Mat src_, ColorCheckerMetric colorchecker_,  int *saturated_threshold_) 
-#define DISTANCE(distance) distance_##distance() 
-#define INITIAL(initial_method) initial_##initial_method
-
-
-class CCM_3x3
-{
-public:
-    struct color_c {
-        Mat dst;
-        char dst_colorspace;
-        char dst_illuminant;
-        char dst_observer;
-        int dst_whites;
-        char colorchecker;
-        char ccm_shape;
-        float* saturated_threshold;
-        char colorspace;
-        char linear;
-        float gamma;
-        float deg;
-        char distance;
-        char dist_illuminant;
-        char dist_observer;
-        Mat weights_list;
-        float weights_coeff;
-        bool weights_color;
-        char initial_method;
-        float xtol;
-        float ftol;
-    } color_ca;
-    Mat src;
-    struct color_c* pcolor_c = &color_ca;
-
-    CCM_3x3(Mat src_, struct color_c* pcolor_c);
-
-    void prepare(void);
-    Mat initial_white_balance(Mat src_rgbl, Mat dst_rgbl);
-    Mat initial_least_square(Mat src_rgbl, Mat dst_rgbl);
-    float loss_rgb(Mat ccm);
-    void calculate_rgb(void);
-    float loss_rgbl(Mat ccm);
-    void calculate_rgbl(void);
-    float loss(Mat ccm);
-    void calculate(void);
-    void value(int number);
-    Mat infer(Mat img, bool L);
-    Mat infer_image(char imgfile, bool L, int inp_size, int out_size, char out_dtype);
-};
-
-CCM_3x3(Mat src_, struct color_c* pcolor_c) {
+CCM_3x3::CCM_3x3(Mat src_, struct color_c* pcolor_c) {
     src = src_;
     dist_io = IO(dist_illuminant_, dist_observer_);
     cs = COLORSPACE(colorspace);
     cs.set_default(dist_io);
-    if (dst) { 
-        cc_ = ColorChecker(dst_, dst_colorspace_, IO(dst_illuminant_, dst_observer_), dst_whites_); 
+    if (dst) {
+        cc_ = ColorChecker(dst_, dst_colorspace_, IO(dst_illuminant_, dst_observer_), dst_whites_);
     }
-    else { 
-        cc_ = COLORCHECKER(colorchecker); 
+    else {
+        cc_ = COLORCHECKER(colorchecker);
     }
     cc = ColorCheckerMetric(cc_, cs, dist_io);
     linear = LINEAR(linear);
     weight = NULL;
-    if (weights_list) { 
+    if (weights_list) {
         weights = weights_list_;
     }
-    elif (weights_coeff!=0){ 
+    elif(weights_coeff != 0) {
         Mat cc_lab_0 = cc.lab.rowRange(0, 1).clone();
         weights = pow(cc_lab_0, weights_coeff_);
     }
@@ -91,36 +30,36 @@ CCM_3x3(Mat src_, struct color_c* pcolor_c) {
     }
 
     saturate_mask = saturate(src, saturated_threshold)
-    mask = saturate_mask & weight_mask
-    src_rgbl = linear.linearize(src)
-    src_rgb_masked = src[mask]
-    src_rgbl_masked = src_rgbl[mask]
-    dst_rgb_masked = cc.rgb[mask]
-    dst_rgbl_masked = cc.rgbl[mask]
-    dst_lab_masked = cc.lab[mask]
-    if (!weights.data)
-    {
-        weights_masked = weights[mask];
-        weights_masked_norm = weights_masked / np.mean(weights_masked);
-    }
+        mask = saturate_mask & weight_mask
+        src_rgbl = linear.linearize(src)
+        src_rgb_masked = src[mask]
+        src_rgbl_masked = src_rgbl[mask]
+        dst_rgb_masked = cc.rgb[mask]
+        dst_rgbl_masked = cc.rgbl[mask]
+        dst_lab_masked = cc.lab[mask]
+        if (!weights.data)
+        {
+            weights_masked = weights[mask];
+            weights_masked_norm = weights_masked / np.mean(weights_masked);
+        }
     masked_len = src_rgb_masked.rows
-    distance = DISTANCE(distance_);
+        distance = DISTANCE(distance_);
     inital_func = INITIAL(initial_method_);
     xtol = xtol_
-    ftol = ftol_
-    ccm = NULL
-    if (distance == "rgb")
-    {
-        calculate_rgb();
-    }
+        ftol = ftol_
+        ccm = NULL
+        if (distance == "rgb")
+        {
+            calculate_rgb();
+        }
     elif(distance == "rgbl")
     {
         calculate_rgbl();
     }
-    else
-    {
-        calculate();
-    }
+        else
+        {
+            calculate();
+        }
 
 }
 
@@ -234,34 +173,26 @@ Mat CCM_3x3::infer(Mat img, bool L) {
         throw "No CCM values!";
     }
     img_lin = linear.linearize(img)
-    img_ccm = img_lin * ccm
-    if (L)
-    {
-        return img_ccm; 
-    }
+        img_ccm = img_lin * ccm
+        if (L)
+        {
+            return img_ccm;
+        }
     return cs.rgbl2rgb(img_ccm);
 }
 
 Mat CCM_3x3::infer_image(char imgfile, bool L, int inp_size, int out_size, char out_dtype) {
     img = imread(imgfile)
-    img = cvtColor(img, img, CV_BGR2RGB) / inp_size
-    out = infer(img, L)
-    //img = np.minimum(np.maximum(np.round(out * out_size), 0), out_size)
-    //img = img.astype(out_dtype)
-    return cvtColor(img, img, CV_RGB2BGR);
+        img = cvtColor(img, img, CV_BGR2RGB) / inp_size
+        out = infer(img, L)
+        //img = np.minimum(np.maximum(np.round(out * out_size), 0), out_size)
+        //img = img.astype(out_dtype)
+        return cvtColor(img, img, CV_RGB2BGR);
 }
 
 
 
-class CCM_4x3 : public CCM_3x3
-{
-public:
-    void prepare(void) {};
-    Mat add_column(Mat arr) {};
-    Mat initial_white_balance(Mat src_rgbl, Mat dst_rgbl) {};
-    Mat infer(Mat img, bool L) {};
-    void value(int number) {};
-};
+
 
 void CCM_4x3::prepare(void) {
     src_rgbl_masked = add_column(src_rgbl_masked);
