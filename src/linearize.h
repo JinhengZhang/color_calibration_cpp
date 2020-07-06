3434ad55efb58fb0fcd540698f141cb56da04207
JinhengZhang <49140379+JinhengZhang@users.noreply.github.com>
2020-07-06 09:13:45 +0800

    Update linearize.h

--- src/linearize.h
+++ src/linearize.h
@@ -12,9 +12,11 @@ class Linear
 public:
     Linear() {}
     Linear(float gamma_, int deg, Mat src, ColorCheckerMetric cc, vector<double> saturated_threshold) {}
-    void calc(void) {}
-    Mat linearize(Mat inp);
-    void value(void) {}
+    virtual void calc(void) {}
+    virtual Mat linearize(Mat inp);
+    virtual void value(void) {}
+    Mat polyfit(Mat src_x, Mat src_y, int order);
+    Mat poly1d(Mat src, Mat w, int deg);
 };
 
 
@@ -29,6 +31,7 @@ class Linear_gamma : public Linear
 {
 public:
     float gamma;
+    Linear_gamma() {};
     Linear_gamma(float gamma_, int deg, Mat src, ColorCheckerMetric cc, vector<double> saturated_threshold);
     Mat linearize(Mat inp);
 };
@@ -97,4 +100,30 @@ public:
 };
 
 
+
+Linear* get_linear(string linear) {
+    Linear* p = new Linear;
+    if (linear == "Linear") {
+        p = new Linear;
+    }
+    else if (linear == "Linear_identity") {
+        p = new Linear_identity;
+    }
+    else if (linear == "Linear_gamma") {
+        p = new Linear_gamma;
+    }
+    else if (linear == "Linear_color_polyfit") {
+        p = new Linear_color_polyfit;
+    }
+    else if (linear == "Linear_color_logpolyfit") {
+        p = new Linear_color_logpolyfit;
+    }
+    else if (linear == "Linear_gray_polyfit") {
+        p = new Linear_gray_polyfit;
+    }
+    else if (linear == "Linear_gray_logpolyfit") {
+        p = new Linear_gray_logpolyfit;
+    }
+    return p;
+}
 #endif
