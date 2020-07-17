7cd8d3be5fc859773a512c4dd078f56cf8c27903
JinhengZhang <49140379+JinhengZhang@users.noreply.github.com>
2020-07-17 16:23:44 +0800

    Update colorchecker.cpp

--- src/colorchecker.cpp
+++ src/colorchecker.cpp
@@ -1,11 +1,14 @@
 #include "colorchecker.h"
 
+//vector<double> white_m(10,1);
 
 ColorChecker::ColorChecker(Mat color, string colorspace, IO io_, Mat whites) {
-	if (colorspace == "lab")
+	//if (colorspace == "lab")
+	if (colorspace == "LAB")
 	{
-		lab = color;
+		lab = color;	
 		io = io_;
+		
 	}
 	else
 	{
@@ -13,17 +16,28 @@ ColorChecker::ColorChecker(Mat color, string colorspace, IO io_, Mat whites) {
 		cs = get_colorspace(colorspace);
 	}
 
-	vector<bool> white_m(color.rows, false);
+	//vector<bool> white_m(color.rows, false);
+	//vector<double> white_m(color.rows, 0);
+	vector<double> white_m(color.rows, 1);
+
+
 	if (!whites.empty())
 	{
-		for (int i = 0; i < whites.cols; i++) 
+		for (int i = 0; i < whites.cols; i++)
 		{
-			white_m[whites.at<double>(0, i)] = true;
+			
+			white_m[whites.at<double>(0, i)] = 0;
+			
 		}
+		this->white_mask =Mat(white_m, true);
 	}
-	Mat white_mask = Mat(white_m);
+	//Mat white_mask;
 
-	color_mask = ~white_mask;
+//	white_mask = Mat(white_m,true);
+	
+	//color_mask = ~Mat(white_m);
+	//color_mask = white_mask;
+	color_mask = Mat(white_m, true);
 }
 
 ColorCheckerMetric::ColorCheckerMetric(ColorChecker colorchecker, string colorspace, IO io_)
@@ -31,8 +45,10 @@ ColorCheckerMetric::ColorCheckerMetric(ColorChecker colorchecker, string colorsp
 	cc = colorchecker;
 	cs = get_colorspace(colorspace);
 	io = io_;
+	
 	if (!cc.lab.empty())
 	{
+		
 		lab = lab2lab(cc.lab, cc.io, io_);
 		xyz = lab2xyz(lab, io_);
 		rgbl = cs->xyz2rgbl(xyz, io_);
@@ -40,13 +56,14 @@ ColorCheckerMetric::ColorCheckerMetric(ColorChecker colorchecker, string colorsp
 	}
 	else
 	{
+		
 		rgb = cs->xyz2rgb(cc.cs->rgb2xyz(cc.rgb, IO("D65", 2)), IO("D65", 2));
 		rgbl = cs->rgb2rgbl(rgb);
 		xyz = cs->rgbl2xyz(rgbl, io);
 		lab = xyz2lab(xyz, io);
 	}
 	grayl = xyz2grayl(xyz);
-	white_mask = cc.white_mask;
-	color_mask = cc.color_mask;
+	this->white_mask = cc.white_mask;
+	this->color_mask = cc.color_mask;
 }
 
