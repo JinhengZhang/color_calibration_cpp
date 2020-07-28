84c7c33a40884e1586c75713faaee5ac8a00018a
JinhengZhang <49140379+JinhengZhang@users.noreply.github.com>
2020-07-28 19:35:31 +0800

    Update linearize.h

--- src/linearize.h
+++ src/linearize.h
@@ -1,7 +1,6 @@
 #ifndef LINEARIZE_H
 #define LINEARIZE_H
 
-#include "utils.h"
 #include "colorchecker.h"
 
 using namespace std;
@@ -11,103 +10,88 @@ class Linear
 {
 public:
     Linear() {}
-    Linear(float gamma_, int deg, Mat src, ColorCheckerMetric cc, vector<double> saturated_threshold) {}
+    Linear(float gamma_, int deg, cv::Mat src, ColorCheckerMetric cc, vector<double> saturated_threshold) {}
     virtual void calc(void) {}
-    virtual Mat linearize(Mat inp);
+    virtual cv::Mat linearize(cv::Mat inp);
     virtual void value(void) {}
-    Mat polyfit(Mat src_x, Mat src_y, int order);
-    Mat poly1d(Mat src, Mat w, int deg);
-    Mat _polyfit(Mat src, Mat dst, int deg);
-    Mat _lin(Mat p, Mat x, int deg);
-    Mat mask_copyto(Mat src, Mat mask);
-    Mat mask_copyto_gray(Mat src, Mat mask);
-
+    cv::Mat polyfit(cv::Mat src_x, cv::Mat src_y, int order);
+    cv::Mat poly1d(cv::Mat src, cv::Mat w, int deg);
+    cv::Mat _polyfit(cv::Mat src, cv::Mat dst, int deg);
+    cv::Mat _lin(cv::Mat p, cv::Mat x, int deg);
 };
 
-
-class Linear_identity : public Linear
+class LinearIdentity : public Linear
 {
 public:
     using Linear::Linear;
 };
 
-
-class Linear_gamma : public Linear
+class LinearGamma : public Linear
 {
 public:
-    float gamma;
-    Linear_gamma() {};
-    Linear_gamma(float gamma_, int deg, Mat src, ColorCheckerMetric cc, vector<double> saturated_threshold);
-    Mat linearize(Mat inp);
+    double gamma;
+    LinearGamma() {};
+    LinearGamma(float gamma_, int deg, cv::Mat src, ColorCheckerMetric cc, vector<double> saturated_threshold);
+    cv::Mat linearize(cv::Mat inp);
 };
 
-
-class Linear_color_polyfit : public Linear
-{
-public:
-    int deg;
-    Mat mask;
-    Mat src;
-    Mat dst;
-    Mat pr, pg, pb;
-    
-    Linear_color_polyfit() {};
-    Linear_color_polyfit(float gamma, int deg, Mat src, ColorCheckerMetric cc, vector<double> saturated_threshold);
-    void calc(void);
-    Mat linearize(Mat inp);
-
-};
-
-
-class Linear_color_logpolyfit : public Linear
+class LinearColorPolyfit : public Linear
 {
 public:
     int deg;
-    Mat mask;
-    Mat src;
-    Mat dst;
-    Mat pr, pg, pb;
-    Linear_color_logpolyfit() {};
-    Linear_color_logpolyfit(float gamma, int deg, Mat src, ColorCheckerMetric cc, vector<double> saturated_threshold);
+    cv::Mat mask;
+    cv::Mat src;
+    cv::Mat dst;
+    cv::Mat pr, pg, pb;
+    LinearColorPolyfit() {};
+    LinearColorPolyfit(float gamma, int deg, cv::Mat src, ColorCheckerMetric cc, vector<double> saturated_threshold);
     void calc(void);
-    Mat linearize(Mat inp);
-   // Mat _polyfit(Mat src, Mat dst, int deg);
-   // Mat _lin(Mat p, Mat x);
+    cv::Mat linearize(cv::Mat inp);
 };
 
-
-class Linear_gray_polyfit : public Linear
+class LinearColorLogpolyfit : public Linear
 {
 public:
     int deg;
-    Mat mask;
-    Mat src;
-    Mat dst;
-    Mat p;
-    Linear_gray_polyfit() {};
-    Linear_gray_polyfit(float gamma, int deg, Mat src, ColorCheckerMetric cc, vector<double> saturated_threshold);
+    cv::Mat mask;
+    cv::Mat src;
+    cv::Mat dst;
+    cv::Mat pr, pg, pb;
+    LinearColorLogpolyfit() {};
+    LinearColorLogpolyfit(float gamma, int deg, cv::Mat src, ColorCheckerMetric cc, vector<double> saturated_threshold);
     void calc(void);
-    Mat linearize(Mat inp);
+    cv::Mat linearize(cv::Mat inp);
 };
 
-
-class Linear_gray_logpolyfit : public Linear
+class LinearGrayPolyfit : public Linear
 {
 public:
     int deg;
-    Mat mask;
-    Mat src;
-    Mat dst;
-    Mat p;
-    Linear_gray_logpolyfit() {};
-    Linear_gray_logpolyfit(float gamma, int deg, Mat src, ColorCheckerMetric cc, vector<double> saturated_threshold);
+    cv::Mat mask;
+    cv::Mat src;
+    cv::Mat dst;
+    cv::Mat p;
+    LinearGrayPolyfit() {};
+    LinearGrayPolyfit(float gamma, int deg, cv::Mat src, ColorCheckerMetric cc, vector<double> saturated_threshold);
     void calc(void);
-    Mat linearize(Mat inp);
-   // Mat _polyfit(Mat src, Mat dst, int deg);
-   // Mat _lin(Mat p, Mat x);
+    cv::Mat linearize(cv::Mat inp);
 };
 
+class LinearGrayLogpolyfit : public Linear
+{
+public:
+    int deg;
+    cv::Mat mask;
+    cv::Mat src;
+    cv::Mat dst;
+    cv::Mat p;
+    LinearGrayLogpolyfit() {};
+    LinearGrayLogpolyfit(float gamma, int deg, cv::Mat src, ColorCheckerMetric cc, vector<double> saturated_threshold);
+    void calc(void);
+    cv::Mat linearize(cv::Mat inp);
+};
 
+Linear* getLinear(string linear, double gamma_, int deg, cv::Mat src, ColorCheckerMetric cc, vector<double> saturated_threshold);
+cv::Mat maskCopyto(cv::Mat src, cv::Mat mask);
 
-Linear* get_linear(string linear);
 #endif
