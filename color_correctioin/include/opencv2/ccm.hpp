3ffce33192a89ad614d222db3a4ad7ad17f9fa89
shanchenqi <582533558@qq.com>
2020-08-26 14:43:42 +0800

    Add files via upload

--- color_correctioin/include/opencv2/ccm.hpp
+++ color_correctioin/include/opencv2/ccm.hpp
@@ -7,7 +7,9 @@
 #include<string>
 #include<vector>
 #include "opencv2/ccm/linearize.hpp"
-
+#include <opencv2/imgproc.hpp>
+#include <valarray>
+#include <opencv2/imgcodecs.hpp>
 
 namespace cv {
     namespace ccm {
